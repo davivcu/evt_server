ff4bfa0cb7f416f536dd3fe82fb89773d3aadc80
davivcu <dav.iv.kovac@gmail.com>
2025-02-18 18:36:28 +0100

    gestione delle rotte vuote

--- routes.py
+++ routes.py
@@ -4,11 +4,15 @@ from config_loader import ANGULAR_PROJECTS
 
 routes = Blueprint("routes", __name__)
 
+@routes.route('/<project_name>')
+@routes.route('/<project_name>/')
 @routes.route("/<project_name>/<path:path>")
-def serve_static(project_name, path):
+def serve_static(project_name, path=''):
     project = next((proj for proj in ANGULAR_PROJECTS if proj["name"] == project_name), None)
     if project:
         dist_path = os.path.join(project["path"], project["build_output"])
+        if not path:
+            return send_from_directory(dist_path, "index.html")
         return send_from_directory(dist_path, path)
     else:
         return "Progetto non trovato", 404
